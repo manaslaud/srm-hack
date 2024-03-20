d1706f114538f54a274c81df0688790c0e9b66c4
manaslaud <manaslaud@hotmail.com>
2024-03-20 19:17:34 +0530

    fix:navbar changes

--- client/src/sections/Hero.tsx
+++ client/src/sections/Hero.tsx
@@ -8,14 +8,14 @@ export default function Hero(){
 }
 function Navbar(){
     return (
-        <nav className="w-full flex justify-between items-center text-white flex-row font-ptMono">
+        <nav className="w-full flex justify-between items-center text-white flex-row font-ptMono border-[1px]  border-[#0f0f0f] rounded-[1rem] px-[1rem] py-[.5rem]" >
                 {/* <div className="">Logo</div> */}
                 <Image className="cursor-pointer" width={100} height={100} src={'/logo.svg'} alt="Logo"/>
-                <div className="w-[40%] flex justify-around items-center text-[0.85rem] cursor-pointer">
+                <div className="w-[40%] flex justify-between items-center text-[0.9rem] cursor-pointer">
                     <span>Home</span>
                     <span>About us</span>
                     <span>Contact Us</span>
-                    <button className="px-[1rem] py-[.5rem] border-[1px]  border-gray-600 rounded-[1rem] transition-all duration-400 hover:text-[#fd6220] hover:border-[#fd6220]">Connect Metamask</button>
+                    <button className="px-[1rem] py-[.5rem]  transition-all duration-400 hover:text-[#fd6220] hover:border-[#fd6220]">Connect Metamask</button>
                 </div>
         </nav>
     )

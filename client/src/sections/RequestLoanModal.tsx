d6de58e37e6429ee177d2c8e2906ce9dbc030317
manaslaud <manaslaud@hotmail.com>
2024-03-20 22:25:10 +0530

    feat:request loan integration

--- client/src/sections/RequestLoanModal.tsx
+++ client/src/sections/RequestLoanModal.tsx
@@ -1,17 +1,49 @@
+import { ChangeEvent, useState } from "react"
+import { convertEtherToWei } from "@/utils/Helpers"
+import { useContracts } from "@/contexts/ContractsContext"
+import { DayToUnix } from "@/utils/Helpers"
 const RequestLoanModal:React.FunctionComponent=()=>{
+    const [ether,setEther]=useState<string>('')
+    const [interest,setInterest]=useState<string>('')
+    const [duration,setDuration]=useState<string>('')
+    const {p2pContract}=useContracts()
+    const handleSubmit=async(formData:any)=>{
+        formData.preventDefault()
+        console.log(formData)
+        if(Number(ether)<0 || Number(duration)<0 || Number(interest)<0) alert('ALl values must be positive')
+        const txn=await p2pContract?.requestLoan(convertEtherToWei(Number(ether).toString()),Number(interest),Number(duration)*24*60*60)
+         await txn.wait()
+         console.log(txn)
+    }
     return (
-        <main className="top-0 w-[75%] flex flex-col gap-[1rem] justify-center items-center z-[1000000000] left-[50%] translate-x-[15%] bg-[#0f0f0f] px-[1rem] py-[0.3rem] rounded-[10px]">
+        <main className="top-0 w-[75%] flex flex-col gap-[1rem] justify-center items-center z-[1000000000] left-[50%] translate-x-[15%] bg-[#0f0f0f] px-[1rem] py-[0.3rem] font-us rounded-[10px]">
             <div className="w-full flex flex-col justify-center items-center">
             <h1 className="text-center text-white font-us w-full text-3xl">Request a loan</h1> 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ducimus tempore minus officiis ?</p>
-        <div className="w-full flex gap-[1rem] justify-center items-center flex-col">
-        <label htmlFor="ether">Ether amount</label>
-        <input type="text" placeholder="Enter ether" name="ether" className="bg-transparent rounded-[1rem] px-[1rem] py-[0.3rem]"/>
-        <label htmlFor="ether">Interest Rate</label>
-        <input type="text" placeholder="Enter interest rate" name="ether" className="bg-transparent rounded-[1rem] px-[1rem] py-[0.3rem]"/>
-        <label htmlFor="ether">Duration in days</label>
-        <input type="text" placeholder="Enter days" name="ether" className="bg-transparent rounded-[1rem] px-[1rem] py-[0.3rem]"/>
+        <p className="font-ptMono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ducimus tempore minus officiis?</p>
+            <form onSubmit={handleSubmit} >
+            <div className="w-full flex gap-[1rem] justify-center items-center flex-col">
+        <div className="flex flex-col gap-[1rem] justify-center items-start">
+        <label htmlFor="ether" className="px-[1rem] py-[0.3rem] font-us">Ether amount</label>
+        <input step={0.00000001} type="number" placeholder="Enter ether" name="ether" className="bg-transparent rounded-[1rem] px-[1rem] py-[0.3rem] " onChange={(e:ChangeEvent<HTMLInputElement>)=>{
+            setEther(e.target.value)
+        }}/>
         </div>
+        <div className="flex flex-col gap-[1rem] justify-center items-start">
+        <label className="px-[1rem] py-[0.3rem] font-us" htmlFor="ether">Interest Rate</label>
+        <input step={0.00000001} type="number" placeholder="Enter interest rate" name="ether" className="bg-transparent rounded-[1rem] px-[1rem] py-[0.3rem] " onChange={(e:ChangeEvent<HTMLInputElement>)=>{
+            setInterest(e.target.value)
+        }}/>
+        </div>
+       <div className="flex flex-col gap-[1rem] justify-center items-start">
+       <label className="px-[1rem] py-[0.3rem] font-us" htmlFor="ether">Duration in days</label>
+        <input step={0.00000001} type="number" placeholder="Enter days" name="ether" className="bg-transparent rounded-[1rem] px-[1rem] py-[0.3rem] " onChange={(e:ChangeEvent<HTMLInputElement>)=>{
+            setDuration(e.target.value)
+        }}/>
+       </div>
+        </div>
+<div className="w-full flex justify-center items-center py-[1rem]">
+<button type="submit" className="px-[0.5rem] py-[0.5rem] rounded-[0.50rem] text-[0.9rem] w-[50%] font-us bg-blue-500">Submit</button>
+</div>            </form>
             </div>
 
         </main>
--- client/src/utils/Helpers.ts
+++ client/src/utils/Helpers.ts
@@ -7,6 +7,17 @@ export function convertEtherToWei(ether:string){
   return ethers.parseUnits(ether,"ether");
 }
 export function unixToDate(unix:number){
-  const dt=new Date(unix)
+  const dt=new Date(Math.ceil(unix*1000))
   return dt;
+}
+export function DayToUnix(days: number): number {
+  const today = new Date();
+  const futureDate = new Date(today); // Create a new Date object
+console.log(today.getDate() + days)
+  futureDate.setDate(today.getDate() + days); // Safely add days
+  console.log(futureDate)
+
+  const unixTimestamp = Math.floor(futureDate.getTime() / 1000);
+
+  return unixTimestamp;
 }

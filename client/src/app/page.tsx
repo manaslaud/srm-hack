4e5294fab1a48b67a9fcebac5f6c287e8b1dc2d8
manaslaud <manaslaud@hotmail.com>
2024-03-20 17:48:14 +0530

    feat:more changes to integration

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,10 @@
+"use client"
 import Image from "next/image";
-
+import { useContracts,ContractsContext } from "@/contexts/ContractsContext";
+// import { useContext } from "react";
 export default function Home() {
+  const contracts = useContracts();
+  console.log(contracts)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
---
+++ client/src/contexts/ContractsContext.tsx
@@ -0,0 +1,61 @@
+// contexts/ContractsContext.js
+"use client"
+import * as React from "react";
+import { createContext, useContext, useEffect, useState } from 'react';
+import { ethers,Contract } from 'ethers';
+import data1 from '../app/artifacts/contracts/LiquidityPool.sol/AMMLiquidityPool.json' 
+import data2 from '../app/artifacts/contracts/LiquidityPool.sol/AMMLiquidityPool.json'
+interface Props{
+    children:React.ReactNode
+}
+interface ContractsContextType {
+    p2pContract: Contract | null;
+    liquidityPoolContract: Contract | null;
+  }
+  export const ContractsContext = createContext<ContractsContextType>({ p2pContract: null, liquidityPoolContract: null });
+
+export const ContractsProvider:React.FunctionComponent<Props> = ({ children }) => {
+    const [contracts, setContracts] = useState<ContractsContextType>({ p2pContract: null, liquidityPoolContract: null });
+
+    useEffect(() => {
+        const initContracts = async () => {
+            if(!window.ethereum){
+                alert('Ethereum support is needed');
+                return;
+            }
+            if(!window.ethereum.isMetaMask){
+                alert('MetaMask support is needed');
+                return;
+            }
+    
+            const provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL);
+            const signer = await provider.getSigner();
+    
+            const contractABIs = {
+                P2P: data1,
+                LiquidityPool: data2,
+            };
+            const contractAddresses = {
+                P2PAddress: '0x6fD906809D5CbD13508ed375C5771D8442Cb7a43',
+                LiquidityPoolAddress: '0xB12A4725Bb46657A5B92b0e533b194e2D65439a5',
+            };
+    
+            const p2pContract = new ethers.Contract(contractAddresses.P2PAddress, contractABIs.P2P.abi, signer);
+            const liquidityPoolContract = new ethers.Contract(contractAddresses.LiquidityPoolAddress, contractABIs.LiquidityPool.abi, signer);
+    
+            setContracts({ p2pContract, liquidityPoolContract });
+        };
+    
+        // Call the async function
+        initContracts();
+    }, []);
+    
+
+    return (
+        <ContractsContext.Provider value={contracts}>
+            {children}
+        </ContractsContext.Provider>
+    );
+};
+
+export const useContracts = () => useContext(ContractsContext);

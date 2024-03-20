9058725c8f4b9975c089a04efc833a466ee15ec7
manaslaud <manaslaud@hotmail.com>
2024-03-20 19:26:30 +0530

    feat:particles added

--- client/src/sections/Hero.tsx
+++ client/src/sections/Hero.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image"
+import Particles from "./Particles"
 export default function Hero(){
     return (
-        <section className="w-full flex flex-col ">
+        <section className="w-full flex flex-col relative overflow-hidden h-screen">
         <Navbar/>
+        <Particles
+            className="absolute inset-0 z-10 animate-fade-in"
+            quantity={1050}
+          />
         </section>
     )
 }
 function Navbar(){
     return (
-        <nav className="w-full flex justify-between items-center text-white flex-row font-ptMono border-[1px]  border-[#0f0f0f] rounded-[1rem] px-[1rem] py-[.5rem]" >
+        <nav className="w-full flex justify-between items-center text-white flex-row font-ptMono border-[1px]  border-[#0f0f0f] rounded-[1rem] px-[1rem] py-[.5rem] bg-[#000] z-[1000000000000000]" >
+           
                 {/* <div className="">Logo</div> */}
                 <Image className="cursor-pointer" width={100} height={100} src={'/logo.svg'} alt="Logo"/>
                 <div className="w-[40%] flex justify-between items-center text-[0.9rem] cursor-pointer">

2e378ee6327be7dfdf0fee00c97818e1c80ae75e
manaslaud <manaslaud@hotmail.com>
2024-03-20 19:53:18 +0530

    feat:linking

--- client/src/sections/Hero.tsx
+++ client/src/sections/Hero.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image"
+import Link from "next/link"
 import Particles from "./Particles"
 export default function Hero(){
     return (
-        <section className="w-full flex flex-col relative overflow-hidden h-screen ">
+        <section className="w-full flex flex-col relative overflow-hidden h-screen " id="hero">
         <Navbar/>
         <Particles
             className="absolute inset-0 z-10 animate-fade-in w-full"
             quantity={250}
           />
           <div className="flex text-white absolute text-5xl top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  font-us align-middle justify-center items-center flex-col z-[1000000000000000] gap-[0.5rem]">
-<p className="text-center">            Lorem ipsum dolor sit <br /> amet consectetur <span className="bg-gradient-to-r from-indigo-700 via-purple-300 to-pink-400 bg-clip-text text-transparent">adipisicing.</span>
+<p className="text-center"> Lorem ipsum dolor sit <br /> amet consectetur <span className="bg-gradient-to-r from-indigo-700 via-purple-300 to-pink-400 bg-clip-text text-transparent">adipisicing.</span>
 </p>
 <button className="text-[0.7rem] px-[1rem] py-[0.5rem] rounded-[1rem] cursor-pointer btn-grad hover:bg-left-bottom z-[1000000000000000]">Connect Metamask</button>
           </div>
@@ -24,9 +25,9 @@ function Navbar(){
                 {/* <div className="">Logo</div> */}
                 <Image className="cursor-pointer" width={100} height={100} src={'/logo.svg'} alt="Logo"/>
                 <div className="w-[40%] flex justify-between items-center text-[0.9rem] cursor-pointer">
-                    <span>Home</span>
-                    <span>About us</span>
-                    <span>Contact Us</span>
+                    <Link href={'/#home'}><span>Home</span></Link>          
+                    <Link href={'/#services'}><span>Services</span></Link>
+                    <Link href={'/#contact'}><span>Contact us</span></Link>
                     <button className="px-[1rem] py-[.5rem]  transition-all duration-400 hover:text-[#fd6220] hover:border-[#fd6220]">Connect Metamask</button>
                 </div>
         </nav>
--- client/src/sections/Services.tsx
+++ client/src/sections/Services.tsx
@@ -4,7 +4,7 @@ import Brain2 from "../utils/Brain2"
 import { Canvas } from "@react-three/fiber"
 export default function Services(){
     return (
-       <section className="w-full flex flex-col justify-center items-center gap-[2rem]">
+       <section className="w-full flex flex-col justify-center items-center gap-[2rem]" id="services">
         <h1 className="text-left text-white font-us w-full text-4xl">Our Services</h1>
          <section className="w-full flex border-[1px] justify-around gap-[3rem] items-center border-[#0f0f0f] rounded-[1rem] z-[1000000000000000] bg-[#000]">
                 <div className="w-[25%] aspect-square">

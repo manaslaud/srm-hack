07aceb51c60e6aed13067c437c278711a874c59d
Manas Laud <119937760+manaslaud@users.noreply.github.com>
2024-04-18 23:28:04 +0530

    Update Hero.tsx

--- client/src/sections/Hero.tsx
+++ client/src/sections/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero(props:HeroProps){
             quantity={250}
           />
           <div className="flex text-white absolute text-5xl top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  font-us align-middle justify-center items-center flex-col z-[1000000000000000] gap-[0.5rem]">
-<p className="text-center"> Lorem ipsum dolor sit <br /> amet consectetur <span className="bg-gradient-to-r from-indigo-700 via-purple-300 to-pink-400 bg-clip-text text-transparent">adipisicing.</span>
+<p className="text-center">Simplifying loading in the <br /> DeFi world <span className="bg-gradient-to-r from-indigo-700 via-purple-300 to-pink-400 bg-clip-text text-transparent">BoltShift.</span>
 </p>
 <button onClick={handleCLick} className="text-[0.7rem] px-[1rem] py-[0.5rem] rounded-[1rem] cursor-pointer btn-grad hover:bg-left-bottom z-[1000000000000000]">{props.isMetamaskConnected?'Start Loaning':'Connect Wallet'}</button>
           </div>
@@ -47,7 +47,6 @@ export function Navbar(props:HeroProps){
                 <Image className="cursor-pointer" width={100} height={100} src={'/logo.svg'} alt="Logo"/>
                 {props.isMetamaskConnected?(<div className="w-[40%] flex justify-between items-center text-[0.9rem] cursor-pointer">
                     <p className="font-us">Wallet:{Cookies.get('currentAddress')}</p>
-                    <Link href={'/pool'}><span>Invest</span></Link>          
                 </div>):(<div className="w-[20%] flex justify-between items-center text-[0.9rem] cursor-pointer">
                     <Link href={'/#home'}><span>Home</span></Link>          
                     <Link href={'/#services'}><span>Services</span></Link>
